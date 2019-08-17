336b2c21f2f7996cd72204d74454a6b8aa9fef42
wyx8267 <369264448@qq.com>
2019-08-17 20:47:39 +0800

    排行榜完成

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,11 @@
         .then(res => res.json())
         .then(render)
 
+    fetch('/json/rank.json')
+        .then(res => res.json())
+        .then(json => json.data.topList)
+        .then(renderTopList)
+
     function render(json) {
         renderSlider(json.data.slider)
         renderRadios(json.data.radioList)
@@ -43,5 +48,34 @@
             <div class="list-title">${list.songListDesc}</div>
         </div>
         `).join('')
+    }
+
+    function renderTopList(list) {
+        document.querySelector('.rank-view .toplist').innerHTML = list.map(item => `
+        <li class="top-item">
+            <div class="top-item-media">
+                <a href="#">
+                    <img class="lazyload" data-src="${item.picUrl}">
+                </a>
+            </div>
+            <div class="top-item-info">
+                <h3 class="top-item-title ellipsis">${item.topTitle}</h3>
+                <ul class="top-item-list">
+                    ${songlist(item.songList)}
+                </ul>
+            </div>
+        </li>
+        `).join('')
+
+        lazyload(document.querySelectorAll('.rank-view .toplist .lazyload'))
+
+        function songlist(songs) {
+            return songs.map((song, i) => `
+                <li class="top-item-song">
+                    <i class="song-index">${i + 1}</i>
+                    <span class="song-name">${song.songname}</span>- ${song.singername}
+                </li>
+            `).join('')
+        }
     }
 })()
