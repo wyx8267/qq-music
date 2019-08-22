c4630388a8a2f8a2ea024d382c261cdbe3667cb5
wyx8267 <369264448@qq.com>
2019-08-22 21:43:15 +0800

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+    entry: './scripts/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
